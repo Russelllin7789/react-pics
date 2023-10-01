02de74ef88ad006e7cabb9a9d6641a43098adf10
Russell Lin <superbr7789@gmail.com>
2023-10-01 20:47:39 +0800

    add pages and components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="bg-blue-500 text-white">Russell Lin 2023</div>;
+};
+
+export default Footer;
---
+++ src/pages/Page404.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page404 = () => {
+  return <div>This Page doesn't exist.</div>;
+};
+
+export default Page404;
